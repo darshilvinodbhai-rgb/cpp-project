dc81063fe8745dc56578d14cd208c3beba666bf0
Darshil chaudhry <darshilvinodbhai@gmail.com>
2026-03-24 14:58:22 +0530

    Added new files

--- stu.cpp
+++ stu.cpp
@@ -2,30 +2,30 @@
 using namespace std;
 
 int main(){
+
     int StudentId;
     string StudentName;
     float Percentage;
     string Grade;
 
-    cout<<"Enter StudentId:";
+    cout<<"Enter StudenId:";
     cin>>StudentId;
 
     cout<<"Enter StudentName:";
     cin>>StudentName;
 
-    cout<<"Enter Percentage:";
+    cout<<"Enter Student Percentage:";
     cin>>Percentage;
 
-    cout<<"Enter Grade:";
+    cout<<"Enter Student Grade:";
     cin>>Grade;
 
-
-    cout<<"\n\n---== Student Information ==---"<<endl;
+    cout<<"\n\n--==Student Information==--"<<endl;
     cout<<"StudentId:"<<StudentId<<endl;
     cout<<"StudentName:"<<StudentName<<endl;
     cout<<"Percentage:"<<Percentage<<endl;
     cout<<"Grade:"<<Grade<<endl;
 
-    return 0;
 
+    return 0; 
 }
