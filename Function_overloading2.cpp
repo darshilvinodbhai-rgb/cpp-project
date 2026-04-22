1546573f3f0d1121f04b5e457b18e2d6f465f83b
Darshil chaudhry <darshilvinodbhai@gmail.com>
2026-04-22 15:12:42 +0530

    fo2

---
+++ Function_overloading2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class authsystem{
+
+    public:
+       void login(string email,string password){
+        cout<<"Login with email & password successfully..";
+       }
+
+       void login(string mobileno,long int otp){
+        cout<<"Login with number and otp successfully..";
+       }
+};
+int main(){
+    authsystem a;
+
+    // a.login("darshil@gmail.com","darshil@2004");
+    a.login("7984523299",255015);
+
+
+    return 0;
+}
