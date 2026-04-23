25f32220e8165dbe29c247dd84a7d2e292997c57
Darshil chaudhry <darshilvinodbhai@gmail.com>
2026-04-23 16:21:57 +0530

    virtual

--- virtual_function.cpp
+++ virtual_function.cpp
@@ -1,6 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    return 0
+class cricket
+{
+public:
+    virtual void getTotalOvers()
+    {
+        cout << "Cricket Match start..";
+    }
+};
+class T20match : public cricket
+{
+public:
+    void getTotalOvers()
+    {
+        cout << "T20 match total overs : 20..";
+    }
+};
+class TestMatch : public cricket
+{
+public:
+    void getTotalOvers()
+    {
+        cout << "\nTest match total over : 90 per day...";
+    }
+};
+int main()
+{
+    cricket *c;
+
+    T20match t;
+
+    c = &t;
+    c->getTotalOvers();
+
+    TestMatch test;
+
+    c = &test;
+    c->getTotalOvers();
+
+    return 0;
 }
